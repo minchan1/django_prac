c23932dc13511a1e67ab0412ecd2b91b90e86af1
Minchan <alflscks@naver.com>
2022-01-12 18:17:08 +0900

    0112

---
+++ level1/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+def solution(x):
+#     a,b=0,x
+#     for i in range(4,0,-1):
+#         a += x//10**i
+#         x = x%10**i
+#     print(a)
+#     print(b)    
+#     if b%a == 0 :
+#         return True
+#     else :
+#         return False
+
+# print(solution(11))
