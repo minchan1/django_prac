01543073b8ac026747b8b5473eb194ab5516d9ce
Minchan <alflscks@naver.com>
2022-01-19 20:24:42 +0900

    0110

--- level1/0110.py
+++ level1/0110.py
@@ -6,3 +6,27 @@
 # 최대공약수 : 두 수의 공통된 약수 중에서 가장 큰 수
 # 최소공배수 : 두 수의 공통된 배수 중에서 가장 작은 수
 
+def solution(n, m):
+    a=m if m>n else n
+    b=n if m>n else m
+    ys1 = [i for i in range(1,a+1) if b%i == 0 and a%i ==0]
+    bs1 = [b*i for i in range(1,a+1) for j in range(1,b+1) if b*i == a*j]
+    return [max(ys1),min(bs1)]
+
+
+
+# 과정 
+
+n,m = 24,12
+a=m if m>n else n
+b=n if m>n else m
+ys1 = []
+for i in range(1,a+1):
+    if b%i == 0 and a%i ==0: ys1.append(i) 
+print(max(ys1))
+
+bs1 = []
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        if b*i == a*j: bs1.append(b*i)
+print(min(bs1))
