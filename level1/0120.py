7b978b4e8c360a86c4cd56e8c9579ebd2217dcb2
Minchan <alflscks@naver.com>
2022-01-20 09:46:49 +0900

    0120

---
+++ level1/0120.py
@@ -0,0 +1,8 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12928
+# 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+def solution(n):
+    a = 0
+    for i in range(1,n+1):
+        if n%i ==0: a += i
+    return a
