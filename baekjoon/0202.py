b85e51e01f01c26ade2f490d5a41611679fa7db5
Minchan <alflscks@naver.com>
2022-02-02 18:35:40 +0900

    0202

--- baekjoon/0202.py
+++ baekjoon/0202.py
@@ -32,3 +32,50 @@ def solution(people, limit):
             people.pop()
         cnt += 1
     return cnt
+
+# https://programmers.co.kr/learn/courses/30/lessons/12949
+
+def solution(arr1, arr2):
+    n = len(arr2[0])
+    m = len(arr1)
+    result= [[0]*n for i in range(m)]
+    tmp=0
+    for i in range(m):
+        for j in range(n):
+            for k in range(len(arr1[0])):
+                tmp += arr1[i][k]*arr2[k][j]
+            result[i][j]=tmp
+            tmp=0
+    return result
+
+# https://programmers.co.kr/learn/courses/30/lessons/12909
+
+# 효율성1 실패
+from collections import deque
+s = "(())()"
+s = deque(s)
+if s.count('(') != s.count(')'):
+    print('False')
+elif s[0]==')' or s[-1]=='(':
+    print('False')
+else:
+    while s:
+        if s[0]==')':
+            print('False')
+            break   
+        s.popleft()
+        s.remove(')')
+    print('True')
+
+# 스택 사용
+def solution(s):
+    stack = []
+    for i in s:
+        if i == '(':  # '('는 stack에 추가
+            stack.append(i)
+        else:  # i == ')'인 경우
+            if stack == []:  # 괄호 짝이 ')'로 시작하면 False 반환
+                return False
+            else:
+                stack.pop()  # '('가 ')'와 짝을 이루면 stack에서 '(' 하나 제거
+    return stack==[]
--- 연습장2.py
+++ 연습장2.py
@@ -1,61 +1,15 @@
-
-# import heapq
-# def solution(scoville, K):
-#     answer = 0
-#     scoville.sort()
-#     while scoville[0] < K:
-#         if len(scoville) <= 1:
-#             return -1
-#         else:
-#             small = heapq.heappop(scoville)
-#             small2 = heapq.heappop(scoville)
-#             heapq.heappush(scoville, (small + (small2 * 2)))
-#             answer += 1
-#     return answer
-
-
-
-# numbers = [3, 30, 34, 5, 9]
-# for i in range(len(numbers)):
-#     numbers[i] = str(numbers[i])
-# numbers.sort()
-# loc = []
-# for i in range(len(numbers)-1):
-#     if numbers[i][0] != numbers[i+1][0]:
-#         loc.append(i+1)
-
-# print(loc)
-
-
 from collections import deque
-answer = 0
-people=[70, 50, 80, 50]
-limit=100
-deq = deque(sorted(people))
-# while deq:
-#     if len(deq) == 1:
-#         answer += 1
-#         break
-#     if deq[0] + deq[-1] <= limit:
-#         deq.pop()
-#         deq.popleft()
-#     else:
-#         deq.pop()
-#     answer += 1
-print(deque)
-
-from collections import deque
-def solution(people, limit):
-    answer = 0
-    deq = deque(sorted(people))
-    while deq:
-        if len(deq) == 1:
-            answer += 1
-            break
-        if deq[0] + deq[-1] <= limit:
-            deq.pop()
-            deq.popleft()
-        else:
-            deq.pop()
-        answer += 1
-    return answer
+s = "(())()"
+s = deque(s)
+if s.count('(') != s.count(')'):
+    print('False')
+elif s[0]==')' or s[-1]=='(':
+    print('False')
+else:
+    while s:
+        if s[0]==')':
+            print('False')
+            break   
+        s.popleft()
+        s.remove(')')
+    print('True')
