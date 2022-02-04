4764e6083fa95876733a6694ca0011f1d66766aa
Minchan <alflscks@naver.com>
2022-02-04 17:44:23 +0900

    0203

--- baekjoon/0203.py
+++ baekjoon/0203.py
@@ -16,3 +16,30 @@ for word in words:
     if len(tmp) == len(set(word))-1:
         cnt += 1
 print(cnt)
+
+# 스택,큐
+# https://programmers.co.kr/learn/courses/30/lessons/42586
+
+def solution(progresses, speeds):
+    remain = []
+    result = []
+    cnt = []
+    for i in range(len(progresses)):
+        tmp=(100-progresses[i])/speeds[i]
+        if int(tmp) == tmp:
+            remain.append(int(tmp))
+        else:
+            remain.append(int(tmp)+1)
+    for i in range(len(remain)):
+        if result:
+            if result[0]>=remain[i]:
+                result.append(remain[i])
+            else:
+                cnt.append(len(result))
+                result.clear()
+                result.append(remain[i])
+        else:
+            result.append(remain[i])
+    if result:
+        cnt.append(len(result))
+    return cnt
--- 연습장2.py
+++ 연습장2.py
@@ -1,15 +1,26 @@
-from collections import deque
-s = "(())()"
-s = deque(s)
-if s.count('(') != s.count(')'):
-    print('False')
-elif s[0]==')' or s[-1]=='(':
-    print('False')
-else:
-    while s:
-        if s[0]==')':
-            print('False')
-            break   
-        s.popleft()
-        s.remove(')')
-    print('True')
+# from collections import deque
+progresses = [93, 30, 55]
+speeds = [1, 30, 5]
+remain = []
+result = []
+cnt = []
+for i in range(len(progresses)):
+    tmp=(100-progresses[i])/speeds[i]
+    if int(tmp) == tmp:
+        remain.append(int(tmp))
+    else:
+        remain.append(int(tmp)+1)
+# remain = deque(remain)
+for i in range(len(remain)):
+    if result:
+        if result[0]>=remain[i]:
+            result.append(remain[i])
+        else:
+            cnt.append(len(result))
+            result.clear()
+            result.append(remain[i])
+    else:
+        result.append(remain[i])
+if result:
+    cnt.append(len(result))
+print(cnt)
