6d74bda24ae29096378487489187207ac1a7c175
Minchan <alflscks@naver.com>
2022-03-20 22:21:15 +0900

    0320

--- march/0320.py
+++ march/0320.py
@@ -1,6 +1,26 @@
 # https://www.acmicpc.net/problem/9012
 
 
-item = '())(()'
-if item.count('(') != item.count(')'):
-    print('NO')
+def func(item):
+    if item.count('(') != item.count(')'):
+        print('NO')
+        return
+    if item[0] == ')' or item[-1] == '(':
+        print('NO')
+        return
+    tmp = list(item)
+    while tmp[0] == '(':
+        tmp.remove('(')
+        tmp.remove(')')
+        if not tmp:
+            break
+        if tmp[0] == ')':
+            print('NO')
+            return
+    print('YES')
+
+
+n = int(input())
+for _ in range(n):
+    item = str(input())
+    func(item)
